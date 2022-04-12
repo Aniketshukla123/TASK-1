15201ef0c782a1e7c10b17a447b9e672807b8050
Aniketshukla123 <71010356+Aniketshukla123@users.noreply.github.com>
2022-04-12 23:14:39 +0530

    Add files via upload

---
+++ task#1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE DBQ5;
+create table DBQ5.cars(
+Id int NOT NULL AUTO_INCREMENT primary key,
+Name varchar(50) NOT NULL,
+Language varchar(50) NOT NUll,
+Framework varchar(30) NOT NULL );
